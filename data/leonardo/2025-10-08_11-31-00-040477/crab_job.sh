11430500126a725ee5bd04be273638b7af5ae424
Matteo Marcelletti <mmarcel3@login02.leonardo.local>
2025-10-08 12:06:30 +0200

    Dai forse sono gli ultimi per davvero

---
+++ data/leonardo/2025-10-08_11-31-00-040477/crab_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=crab_tesi_basel
+#SBATCH --output=./data/leonardo/2025-10-08_11-31-00-040477/slurm_output.log
+#SBATCH --error=./data/leonardo/2025-10-08_11-31-00-040477/slurm_error.log
+#SBATCH --nodes=32
+#SBATCH --ntasks-per-node=1
+#SBATCH --account=IscrB_SWING
+#SBATCH --gres=tmpfs:0
+#SBATCH --time=01:00:00
+
+#SBATCH --partition=boost_usr_prod
+module purge
+module load openmpi
+
+source /leonardo/home/userexternal/mmarcel3/crab/.venv/bin/activate
+
+/leonardo/home/userexternal/mmarcel3/crab/.venv/bin/python3 /leonardo/home/userexternal/mmarcel3/crab/cli.py --worker --workdir ./data/leonardo/2025-10-08_11-31-00-040477
